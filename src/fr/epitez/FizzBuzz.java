3d3c0d18586041737c254c468cb0df1285f67456
Jean-François Marronnier <jfmarronnier@epitez.fr>
2020-11-25 23:49:21 +0100

    [VERT] Initialize le champs value

--- src/fr/epitez/FizzBuzz.java
+++ src/fr/epitez/FizzBuzz.java
@@ -2,10 +2,15 @@ package fr.epitez;
 
 public class FizzBuzz {
 	private String value;
-	
+
+	public FizzBuzz() {
+		super();
+		this.value = new String();
+	}
+
 	@Override
 	public String toString() {
-		return "";
+		return this.value;
 	}
 
 }
